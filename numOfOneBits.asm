46c979273e0472c74afe62e6c9eaff4c11634844
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-02-25 10:59:20 +0100

    reversed branch checking counter

--- numOfOneBits.asm
+++ numOfOneBits.asm
@@ -7,6 +7,6 @@ TEST    ADD R0, R0, #0
         ADD R1, R1, #1
 DECR    ADD R0, R0, R0          ; Bitwise leftshift
         ADD R2, R2, #-1
-        BRz TEST
+        BRnp TEST
         TRAP x25
 .END
