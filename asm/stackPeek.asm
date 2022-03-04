ab6f50a1a4c89c1690aec3e239e2525debb6a07f
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2022-03-04 11:54:12 +0100

    added example of how to use stack and fixed errors in stack

--- asm/stackPeek.asm
+++ asm/stackPeek.asm
@@ -3,6 +3,7 @@
 ; program works with a stack consisting of memory locations x3FFF
 ; through 3FFB. R6 is the stack pointer.
 ;
+                .ORIG   x4000
 POP             AND     R5,R5,#0        ; R5 <-- success
                 ST      R1,Save1        ; Save registers that
                 ST      R2,Save2        ; are needed by POP
@@ -21,6 +22,10 @@ PUSH            AND     R5,R5,#0
                 ADD     R2,R6,R1        ; Compare stack pointer to x3FFB
                 BRz     fail_exit       ; Branch if stack is full
 ;
+                ADD     R6,R6,#-1       ; Adjust statck pointer
+                STR     R0,R6,#0        ; The actual "push"
+                BRnzp   success_exit
+;
 PEEK            ST      R1,Save1        ; Save registers that
                 ST      R2,Save2        ; are needed by PEEK
                 LD      R1,EMPTY        ; EMPTY contains -x4000
@@ -30,9 +35,6 @@ PEEK            ST      R1,Save1        ; Save registers that
                 LDR     R0,R6,#0        ; The actual "peek"
                 BRnzp   success_exit
 ;
-                LDR     R0,R6,#0        ; The actual "pop"
-                ADD     R6,R6,#-1       ; Adjust statck pointer
-                STR     R0,R6,#0        ; The actual push
 success_exit    LD      R2,Save2        ; Restore original
                 LD      R1,Save1        ; register values
                 RET
@@ -40,8 +42,10 @@ success_exit    LD      R2,Save2        ; Restore original
 fail_exit       LD      R2,Save2        ; Restore original
                 LD      R1,Save1        ; register values
                 ADD     R5,R5,#1        ; R5 <-- failure
+                RET
 ;
 EMPTY           .FILL   xC000           ; EMPTY contains -x4000
 FULL            .FILL   xC005           ; FULL contains -x3FFB
 Save1           .FILL   x0000
 Save2           .FILL   x0000
+                .END
---
+++ asm/useOfStack.asm
@@ -0,0 +1,15 @@
+.ORIG x3000
+        AND     R0,R0,#0
+        ADD     R0,R0,#10
+        LD      R6,POP
+        LD      R4,PUSH
+        JSRR    R4
+        AND     R0,R0,#
+        LD      R4,POP
+        JSRR    R4
+        HALT
+
+POP     .FILL x4000
+PUSH    .FILL x4009
+PEEK    .FILL x400E
+.END
